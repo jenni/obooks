b5918827d75b4051f33b3a0b1802c52e29a2ae0c
Jen Varela Lijó <jen.lijo@zalando.de>
2020-11-21 07:34:24 +0100

    Small refactor

--- cli.js
+++ cli.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const { existsSync } = require('fs')
+const { existsSync, writeFileSync } = require('fs')
 
 const OBook = require('./lib/OBook')
 const { getCachedCookies } = require('./lib/Authentication')
-const { write } = require('./lib/utils')
 
 const SESSION_PATH = './session.json'
 
@@ -46,7 +45,7 @@ console.log('\x1b[1m\x1b[33m', `
 
 program
     .version("1.0.0")
-    .option('-b, --bookid <bookid>','[required] OReilly | SafariBooksOnline book identifier')
+    .option('-b, --bookid <bookid>','[required] OReilly book identifier')
     .option('-c, --cookie <cookie>','Session cookies')
     .parse(process.argv)
 
@@ -54,7 +53,7 @@ program
 const sessionCached = (program.bookid && existsSync(SESSION_PATH))
 const sessionAsArg = (program.bookid && program.cookie)
 
-const download = async ({ session, bookid }) => {
+const downloadBook = async ({ session, bookid }) => {
 
     const cookie = getCachedCookies(session)
 
@@ -62,24 +61,22 @@ const download = async ({ session, bookid }) => {
     return ohBooks.create()
 }
 
-
 if (sessionCached) {
-
-    return download({ session: SESSION_PATH, bookid: program.bookid })
-
-} else if (sessionAsArg) {
-
-    write(SESSION_PATH, { cookie: program.cookie }, true)
-
-    return download({ session: SESSION_PATH, bookid: program.bookid })
-
-} else {
-
-    console.log(`\x1b[1m\x1b[31m
-        Something is missing. 
-        Make sure to enter -b <bookid> and -c <cookie>.
-        Example: \x1b[0m $ obooks -b "121212121212" -c "LONG COOKIE STRING HERE"
-        `, '\x1b[0m')
-
-    process.exit(1)
+    return downloadBook({ session: SESSION_PATH, bookid: program.bookid })
 }
+if (sessionAsArg) {
+    writeFileSync(
+        SESSION_PATH,
+        JSON.stringify({ cookie: program.cookie }, undefined, 2),
+        'utf8'
+    )
+    return downloadBook({ session: SESSION_PATH, bookid: program.bookid })
+}
+
+console.log(`\x1b[1m\x1b[31m
+    Something is missing.
+    Make sure to enter -b <bookid> and -c <cookie>.
+    Example: \x1b[0m $ obooks -b "121212121212" -c "LONG COOKIE STRING HERE"
+    `, '\x1b[0m')
+
+process.exit(1)
